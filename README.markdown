8016559245d44a282bee1a16c52390f01bd35d27
James Turnbull <james@lovedthanlost.net>
2010-04-23 12:56:56 +1000

    Initial version

---
+++ README.markdown
@@ -0,0 +1,36 @@
+# pastie plugin for Rbot
+# Copyright @ 2010 James Turnbull <james@lovedthanlost.net>
+# Licensed: GPLv3
+# Needs to go in the plugins dir for rbot
+
+Pastie Plugin for Rbot
+======================
+
+Requires
+--------
+
+Requires the following:
+
+* net/http
+* uri
+
+Install
+-------
+
+1.  Copy into the Rbot plugins directory
+2.  Add it to list of loaded Rbot plugins
+3.  Restart Rbot
+
+Usage
+-----
+
+1.  Ask the bot to provide you with a Pastie.org URL with one of the following commands:
+
+  botname: pastie
+  botname: pastie me
+  botname: url me
+
+2.  The bot will send you a private message with a Pastie.org URL.
+
+3.  Click on the link and use the "Paste Again" button to edit your pastie.
+ 
---
+++ pastie.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'net/http'
+require 'uri'
+
+class PastiePlugin < Plugin
+        
+	def help(plugin, topic = "")
+		case topic
+			when '':
+				"pastie: Manages pastie paste requests. " +
+                                "Ask the bot for a pastie link using: " +
+                                "'pastie', 'pastie me', or 'url me'. " +
+                                "For example, 'botname: url me'. "
+		end
+	end
+	
+	def listen(m)
+		# Ignore things unless we're directly talked to
+		return unless m.address?
+	
+                # Return unless we're addressed properly	
+		return unless m.message =~ /(pastie|pastie me|url me)/
+
+                if m.message =~ /^(\S+)[:,]/
+	          addressee = "#{$1}: "
+                else
+		  addressee = "#{m.sourcenick}: "
+	        end
+		
+                pastie
+ 
+                m.reply "#{addressee} Your pastie URL " + @url  
+	end
+
+	private
+        def pastie
+                pastie_url = URI.parse("http://pastie.org/pastes")
+
+                req = Net::HTTP::Post.new(pastie_url.path)
+   
+                body = "Click Paste Again to edit"
+ 
+                form_data = {"paste[body]" => body, "paste[authorization]" => "burger"}
+    
+                req.set_form_data(form_data)
+                req.add_field("User-Agent", "rbot pastie plugin")
+    
+                result = Net::HTTP.new(pastie_url.host, pastie_url.port).start {|http| http.request(req) }
+                
+                @url = result['location']
+        end
+end
+
+plugin = PastiePlugin.new
